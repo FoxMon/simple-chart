d8f35bf407add9c52ab48bb25efff98dda4a5b66
foxmon <jjockrod@naver.com>
2023-03-03 21:56:10 +0900

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple chart react
+
+## Project
+
+## 1. Project start for dev mode
+
+```
+npm run start
+```
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 3000,
+  },
+  module: {
+    // exclude node_modules
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  // JS files will path through Babel
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+};
